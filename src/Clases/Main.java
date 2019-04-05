89e7c3f572951f34324f231aacd42fe547fc16d3
6001231 <6001231@PC0YWDUB.insags.com>
2019-04-05 13:22:05 +0200

    --ESTABLE 0.1---

---
+++ src/Clases/Main.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 6001231
+ */
+public class Main extends Application {
+
+    public static Scene scene;
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+       Logica l = new Logica (primaryStage);
+    }
+
+}
--- src/Controllers/FXMLMainController.java
+++ src/Controllers/FXMLMainController.java
@@ -8,28 +8,60 @@ package Controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  *
  * @author 6001231
  */
 public class FXMLMainController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
+    private JFXTextField etUsuario;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private JFXButton btnFirmar;
+
+    @FXML
+    private Label lbResultadoComprobacion;
+
+    @FXML
+    private JFXButton btnCargarVistaAdmin;
+
+    @FXML
+    void cargarVistaAdmin(ActionEvent event) throws IOException {
+
+        Stage stage = new Stage();
+         Parent root = FXMLLoader.load(getClass().getResource("/XMLDocuments/vistaAdmin.fxml"));
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        
+        stage.setTitle("Nueva Ventana");
+        stage.show();
+
     }
-    
+
+    @FXML
+    void comprobarFirma(ActionEvent event) {
+        if (etUsuario.getText().equals("David")) {
+            lbResultadoComprobacion.setText("Correcto");
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
