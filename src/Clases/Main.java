e6dcee5a98a3ccb2ea8271d00622de97ea06cf13
davidscld <33543418+davidscld@users.noreply.github.com>
2019-04-05 22:32:27 +0200

    ---Estable 0.3---

--- src/Clases/Main.java
+++ src/Clases/Main.java
@@ -23,7 +23,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Logica l = new Logica(primaryStage);
+       Logica l = new Logica(primaryStage);
     }
 
 }
