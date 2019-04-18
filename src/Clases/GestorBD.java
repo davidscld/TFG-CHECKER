2714fa02421be39863e180fffb8922e58396a4e7
davidscld <33543418+davidscld@users.noreply.github.com>
2019-04-18 21:27:41 +0200

    ---Estable 1.2.0--- Añade empleados , añade horarios

--- src/Clases/GestorBD.java
+++ src/Clases/GestorBD.java
@@ -5,11 +5,14 @@
  */
 package Clases;
 
+import java.io.FileInputStream;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Time;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.logging.Level;
@@ -29,11 +32,12 @@ public class GestorBD {
     private String driver = "com.mysql.jdbc.Driver";
     private String url = "jdbc:mysql:" + origenDatos;
     private Statement st;
-    private ResultSet rs;
+    private ResultSet resultadoSelect;
     private String sentenciaSQL;
     private final String NOMBRE_TABLA_TRBAJADORES = "trabajadores", NOMBRE_TABLA_HORARIOS = "horarios";
     private final String RESULTADO_OK = "Operacion OK", RESULTADO_KO = "Operacion fallida";
     private int resultadoConsultaSQL;
+    private static ArrayList<Integer> listadoNumerosTrabajadores = listadoNumerosTrabajadores = new ArrayList<>();
 
     public GestorBD() {
         try {
@@ -43,8 +47,79 @@ public class GestorBD {
         }
     }
 
-    public void darAltaTrabajador() {
+    public String eliminarHorarioTrabajador(int codigoTrabajador, Date fecha) {
+        try {
+            conexion = DriverManager.getConnection(url, usuario, contrasenia);
+            st = conexion.createStatement();
 
+            sentenciaSQL = "DELETE FROM " + NOMBRE_TABLA_HORARIOS + " WHERE numeroEmpleado = " + codigoTrabajador + " AND fecha = '" + fecha + "';";
+
+            resultadoConsultaSQL = st.executeUpdate(sentenciaSQL);
+
+            st.close();
+            conexion.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(GestorBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        if (resultadoConsultaSQL == 1) {
+            return RESULTADO_OK;
+        } else {
+            return RESULTADO_KO;
+
+        }
+    }
+
+    public String nuevoHorarioTrabajador(int codigoTrabajador, Time horaInicio, Time horaFin, Date fecha) {
+        try {
+            conexion = DriverManager.getConnection(url, usuario, contrasenia);
+            st = conexion.createStatement();
+
+            sentenciaSQL = "INSERT INTO " + NOMBRE_TABLA_HORARIOS + "(fecha, horaInicio, horaFin, numeroEmpleado) VALUES ('"
+                    + fecha + "', '" + horaInicio + "', '" + horaFin + "', '" + codigoTrabajador + "');";
+
+            resultadoConsultaSQL = st.executeUpdate(sentenciaSQL);
+
+            st.close();
+            conexion.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(GestorBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        if (resultadoConsultaSQL == 1) {
+            return RESULTADO_OK;
+        } else {
+            return RESULTADO_KO;
+
+        }
+    }
+
+    public String darAltaTrabajador(String nombre, String apellido, FileInputStream imagenFirma) throws SQLException {
+        int codigoEmpleado = nuevoCodigoEmpleado();
+        try {
+            conexion = DriverManager.getConnection(url, usuario, contrasenia);
+            st = conexion.createStatement();
+
+            sentenciaSQL = "INSERT INTO " + NOMBRE_TABLA_TRBAJADORES + "(nombre, apellidos, numeroEmpleado, firma) VALUES ('"
+                    + nombre + "', '" + apellido + "', '" + codigoEmpleado + "', '" + imagenFirma + "');";
+
+            resultadoConsultaSQL = st.executeUpdate(sentenciaSQL);
+
+            st.close();
+            conexion.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(GestorBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        if (resultadoConsultaSQL == 1) {
+            return RESULTADO_OK;
+        } else {
+            return RESULTADO_KO;
+
+        }
     }
 
     public String darBajaTrabajador(int codigoTrabajador) {
@@ -71,33 +146,49 @@ public class GestorBD {
         }
     }
 
-    public String eliminarHorarioTrabajador(int codigoTrabajador, LocalDate fecha) {
+    public ArrayList<Trabajador> listadoDeTrabajadores() {
+
+        return null;
+    }
+
+    private int nuevoCodigoEmpleado() throws SQLException {
+        int codigoEmpleado;
+        boolean numeroEncontrado;
+        if (listadoNumerosTrabajadores.isEmpty()) {
+            cargarArrayListNumerosTrabajadores();
+        }
+        do {
+            numeroEncontrado = false;
+            codigoEmpleado = (int) (Math.random() * 1000);
+            for (Integer it : listadoNumerosTrabajadores) {
+                if (it == codigoEmpleado) {
+                    numeroEncontrado = true;
+                }
+            }
+
+        } while (numeroEncontrado);
+
+        return codigoEmpleado;
+    }
+
+    public void cargarArrayListNumerosTrabajadores() {
+
         try {
-            conexion = DriverManager.getConnection(url, usuario, contrasenia);
-            st = conexion.createStatement();
+            conexion = DriverManager.getConnection(url, usuario, contrasenia);  //SIEMPRE IGUAL
+            st = conexion.createStatement();    //SIEMPRE IGUAL
 
-            sentenciaSQL = "DELETE FROM " + NOMBRE_TABLA_HORARIOS + " WHERE numeroEmpleado = " + codigoTrabajador + " AND fecha = '" + fecha + "';";
-
-            resultadoConsultaSQL = st.executeUpdate(sentenciaSQL);
-
-            st.close();
-            conexion.close();
+            //Cambiamos la sentenncia que vamos a ejecutar, este caso sera un UPDATE            
+            sentenciaSQL = "SELECT numeroEmpleado FROM " + NOMBRE_TABLA_TRBAJADORES + ";";
+            resultadoSelect = st.executeQuery(sentenciaSQL);
+            while (resultadoSelect.next()) {
+                listadoNumerosTrabajadores.add(Integer.parseInt(resultadoSelect.getString(1)));
+            }//while "hay mas datos"           
+            st.close(); //cerramos
+            conexion.close();   //cerramos
 
         } catch (SQLException ex) {
             Logger.getLogger(GestorBD.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        if (resultadoConsultaSQL == 1) {
-            return RESULTADO_OK;
-        } else {
-            return RESULTADO_KO;
-
-        }
-    }
-
-    public ArrayList<Trabajador> listadoDeTrabajadores() {
-
-        return null;
     }
 
 }
